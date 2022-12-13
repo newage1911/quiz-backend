8d1080e38150d968f8ed76d6688cd89801fbaeaf
March <teerachot.ngern@gmail.com>
2022-12-13 15:21:59 +0700

    edit service score

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,40 +1,54 @@
-import { Controller, Get, Post, Body, Param, Patch } from "@nestjs/common";
-import { Quiz } from "../entities/quiz.entity";
-import { User } from "../entities/user.entity";
-import { Answer } from "../entities/answer.entity";
-import { quizService } from "./quiz.service";
-
-@Controller()
-export class quizController {
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Patch,
+    Delete,
+  } from '@nestjs/common';
+  import { Quiz } from '../entities/quiz.entity';
+  import { User } from '../entities/user.entity';
+  import { Answer } from '../entities/answer.entity';
+  import { quizService } from './quiz.service';
+  
+  @Controller()
+  export class quizController {
     constructor(private quizService: quizService) {}
-
+  
     @Get()
     getQuiz() {
-        return this.quizService.getQuiz();
+      return this.quizService.getQuiz();
     }
-
+  
     @Get('score')
     getScore(): Promise<Quiz[]> {
-        return this.quizService.getScoreBoard();
+      return this.quizService.getScoreBoard();
     }
-
+  
     @Post()
-    create(@Body() quiz:Quiz) {
-        return this.quizService.addQuiz(quiz);
+    create(@Body() quiz: Quiz) {
+      return this.quizService.addQuiz(quiz);
     }
-
+  
     @Patch()
-    update(@Body() quiz:Quiz) {
-        return this.quizService.updateQuiz(quiz);
+    update(@Body() quiz: Quiz) {
+      return this.quizService.updateQuiz(quiz);
     }
-
+  
     @Get('team/:uid')
     async findTeam(@Param('uid') uid: string): Promise<User> {
-        return await this.quizService.findTeamByUid(uid);
+      return await this.quizService.findTeamByUid(uid);
     }
-
+  
     @Get('answer/:id')
     async findAnswer(@Param('id') id: number): Promise<Answer> {
-        return await this.quizService.findOneById(id);
+      return await this.quizService.findOneById(id);
     }
-}
+  
+    @Delete('reset')
+    deleteAllQuiz() {
+      return this.quizService.deleteAllQuiz();
+    }
+  }
+  
